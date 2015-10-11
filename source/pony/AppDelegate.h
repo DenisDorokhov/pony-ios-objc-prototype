f1c0a044f4eb13d61ba8efad122edc8983c17bd9
DenisDorokhov <denis@dorokhov.net>
2015-10-11 13:22:40 +0200

    create xcode / pods project

---
+++ source/pony/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pony
+//
+//  Created by Denis Dorokhov on 11/10/15.
+//  Copyright © 2015 Denis Dorokhov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ source/pony/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pony
+//
+//  Created by Denis Dorokhov on 11/10/15.
+//  Copyright © 2015 Denis Dorokhov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
